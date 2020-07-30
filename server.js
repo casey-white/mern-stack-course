f091279e2539972ab49d4825e998ead7e05dddda
Casey White <cwhite2019@gmail.com>
2020-07-30 18:13:55 -0400

    route modules boilerplate

--- server.js
+++ server.js
@@ -12,4 +12,10 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
