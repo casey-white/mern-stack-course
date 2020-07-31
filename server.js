150f218cadad2b8247e343e9fc49a95437d6dadd
Casey White <cwhite2019@gmail.com>
2020-07-30 20:02:02 -0400

    validation example

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // connect Db
 connectDb();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
